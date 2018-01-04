2a0a237594b93535178aeb9a45a3a55b2d27ae5f
亚派 <1025590386@qq.com>
2018-01-04 15:17:50 +0800

    treetableview

---
+++ TreeTableView/View/TreeTableView.h
@@ -0,0 +1,32 @@
+//
+//  TreeTableView.h
+//  TreeTableView
+//
+//  Created by yixiang on 15/7/3.
+//  Copyright (c) 2015年 yixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Node;
+
+@protocol TreeTableCellDelegate <NSObject>
+
+/** 点击哪个节点的代理方法.
+ @param node 传出来的数据模型.
+ @param reached 是否到达叶子节点(YES 为到达了, 你可以进行push 之类操作).
+ @return void.
+ */
+-(void)AddNode:(Node *)node;
+-(void)RemoveNode:(Node*)node;
+
+
+
+@end
+
+@interface TreeTableView : UITableView
+
+@property (nonatomic , weak) id<TreeTableCellDelegate> treeTableCellDelegate;
+
+-(instancetype)initWithFrame:(CGRect)frame withData : (NSArray *)data;
+
+@end
